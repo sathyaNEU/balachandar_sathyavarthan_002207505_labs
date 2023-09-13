96bb476fe6cdee9c63cf9abba80bdfe8a6516ba7
bsv-2001 <sathya30122001@gmail.com>
2023-09-13 03:02:14 -0400

    Create/View Product App - Java Swing - Lab1

---
+++ Lab1/JavaSwingLab1/src/Business/Product.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business;
+
+/**
+ *
+ * @author Sathya
+ */
+public class Product {
+    String name;
+    String price;
+    String desc;
+    String avai;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getAvai() {
+        return avai;
+    }
+
+    public void setAvai(String avai) {
+        this.avai = avai;
+    }
+   
+}
